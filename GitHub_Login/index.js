3761e8e0e76c1d6c939e405ec50057d927c43b24
Tlholo-Koma <tlholo@bbd.co.za>
2023-01-18 11:16:22 +0200

    add in script

--- GitHub_Login/index.js
+++ GitHub_Login/index.js
@@ -1,3 +1,10 @@
+const url = "https://github.com"
+
+
 function connect() {
-    
+    fetch(url+"/login/oauth/authorize", {
+        method: "GET",
+    }).then(
+        console.log("Redirected")
+    )
 }
